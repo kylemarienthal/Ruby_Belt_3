96685f10b592839d2a8802caaec637252fd14c81
Kyle Marienthal <kylemarienthal@gmail.com>
2017-10-25 21:16:07 -0500

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+
+  before_save :downcase
+
+  validates :first_name, :last_name, presence: true
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+
+  def downcase
+    self.first_name.downcase!
+    self.last_name.downcase!
+    self.email.downcase!
+  end
+
+end
